eecc28c6840abe37c0402795c7a402743b7d199c
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-03 15:32:49 +0530

    forgot mongoose import. changed typo

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
+import mongoose from "mongoose";
+
 export const connectDB = async () => {
   try {
-    await mongoose.connectDB(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connection Initialized!`);
   } catch (error) {
     console.error("Database connection error:", error);
