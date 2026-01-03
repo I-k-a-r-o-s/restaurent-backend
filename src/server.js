a040cb7588d24034042770da37cff0c198d43b1c
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-03 15:42:50 +0530

    added required middleware and established database connection

--- src/server.js
+++ src/server.js
@@ -1,2 +1,29 @@
+import dotenv from'dotenv'
+dotenv.config()
+
+import express from'express'
+import cors from'cors'
+import cookieParser from 'cookie-parser'
+import { connectDB } from './config/database.js'
 
 const server=express()
+
+//middleware
+server.use(express.json())
+server.use(cors())
+server.use(cookieParser())
+
+const port=process.env.PORT||5000
+
+const startDBConnection=async()=>{
+    try {
+        await connectDB();
+        server.listen(port, () => {
+            console.log(`Server is running on port: ${port}`);
+        });
+    } catch (error) {
+        console.log("MongoDB Connection Failed! :",error)
+        process.exit(1);
+    }
+}
+startDBConnection();
