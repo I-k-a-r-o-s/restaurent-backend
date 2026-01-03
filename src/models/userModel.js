66376725a72bded88ebe68217d44df38f30bfe9f
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-03 15:50:10 +0530

    finished writing user model

---
+++ src/models/userModel.js
@@ -0,0 +1,13 @@
+import { Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
