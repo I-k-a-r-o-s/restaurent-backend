be8ecdd813a0eff7ac0c9913f66e6a69de6e66ab
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-05 10:28:02 +0530

    fixed spacing

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { adminLogin, loginUser, logoutUser, registerUser } from "../controllers/authController.js";
+import {
+  adminLogin,
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/authController.js";
 
-const authRoutes=Router();
+const authRoutes = Router();
 
 authRoutes.post("/register", registerUser);
 authRoutes.post("/login", loginUser);
-authRoutes.post("/admin/login",adminLogin)
-authRoutes.post("/logout",logoutUser)
+authRoutes.post("/admin/login", adminLogin);
+authRoutes.post("/logout", logoutUser);
 
 export default authRoutes;
--- src/server.js
+++ src/server.js
@@ -1,32 +1,32 @@
-import dotenv from'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-import express from'express'
-import cors from'cors'
-import cookieParser from 'cookie-parser'
-import { connectDB } from './config/database.js'
-import authRoutes from './routes/authRoutes.js'
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/database.js";
+import authRoutes from "./routes/authRoutes.js";
 
-const server=express()
+const server = express();
 
 //middleware
-server.use(express.json())
-server.use(cors())
-server.use(cookieParser())
+server.use(express.json());
+server.use(cors());
+server.use(cookieParser());
 
-const port=process.env.PORT||5000
+const port = process.env.PORT || 5000;
 
-server.use("/api/auth",authRoutes)
+server.use("/api/auth", authRoutes);
 
-const startDBConnection=async()=>{
-    try {
-        await connectDB();
-        server.listen(port, () => {
-            console.log(`Server is running on port: ${port}`);
-        });
-    } catch (error) {
-        console.log("MongoDB Connection Failed! :",error)
-        process.exit(1);
-    }
-}
+const startDBConnection = async () => {
+  try {
+    await connectDB();
+    server.listen(port, () => {
+      console.log(`Server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log("MongoDB Connection Failed! :", error);
+    process.exit(1);
+  }
+};
 startDBConnection();
