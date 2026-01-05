ab04b179d0b3ee5a2c9aee43b1b0eaf017b0db9b
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-05 14:36:38 +0530

    wrote category routes

---
+++ src/routes/categoryRoutes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { upload } from "../middleware/multer.js";
+import { adminOnly } from "../middleware/authMiddleWare.js";
+import {
+  addCategory,
+  deleteCategory,
+  getAllCategories,
+  updateCategory,
+} from "../controllers/categoryController.js";
+
+const categoryRoutes = Router();
+
+categoryRoutes.post("/add", adminOnly, upload.single("image"), addCategory);
+categoryRoutes.get("/add", getAllCategories);
+categoryRoutes.put(
+  "/update/:id",
+  adminOnly,
+  upload.single("image"),
+  updateCategory
+);
+categoryRoutes.delete("/delete/:id", adminOnly, deleteCategory);
+
+export default categoryRoutes;
