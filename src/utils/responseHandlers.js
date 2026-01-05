8b476378aab0e958ddf3ec890a21326d6d21cb1b
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-05 14:02:20 +0530

    added more response handlers and reworked existing ones

--- src/utils/responseHandlers.js
+++ src/utils/responseHandlers.js
@@ -1,23 +1,44 @@
 // Failure Response Helper
-export const failure = (res) => {
+export const failure = (res, message = "Internal Server Error") => {
   res.status(500).json({
-    message: "Internal Server Error",
+    message,
     success: false,
   });
 };
 
+export const succcessResponse=(res,status,message="Success")=>{
+  res.status(status).json({
+    message,
+    success: true,
+  });
+}
+
 // Missing Fields Response Helper
-export const missingResponse = (res) => {
+export const missingResponse = (res, message = "Please provide all required fields") => {
   res.status(400).json({
-    message: "Please provide all required fields",
+    message,
     success: false,
   });
 };
 
 // Invalid Credentials Response Helper
-export const invalidResponse = (res) => {
+export const invalidResponse = (res, message = "Invalid Credentials") => {
   res.status(401).json({
-    message: "Invalid Credentials",
+    message,
     success: false,
   });
 };
+
+export const alreadyExistsResponse = (res, message = "Already exists") => {
+  res.status(409).json({
+    message,
+    success: false,
+  });
+};
+
+export const notFoundResponse = (res, message = "Not found") => {
+  res.status(404).json({
+    message,
+    success: false,
+  });
+}
