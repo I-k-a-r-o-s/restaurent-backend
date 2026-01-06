072bf47fa899442d7ecbf4453c9850781f9594c3
I-k-a-r-o-s <141158707+I-k-a-r-o-s@users.noreply.github.com>
2026-01-06 13:05:09 +0530

    wrote order model

---
+++ src/models/orderModel.js
@@ -0,0 +1,45 @@
+import { model, Schema } from "mongoose";
+
+const orderSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      re: "User",
+      required: true,
+    },
+    items: [
+      {
+        menuItem: {
+          type: Schema.Types.ObjectId,
+          ref: "Menu", // Reference to the Menu model
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    totalAmount: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "Preparing", "Delivered"],
+      default: "Pending",
+    },
+    paymentMethod: {
+      type: String,
+      default: "Cash on Delivery",
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = model("Order", orderSchema);
+export default Order;
